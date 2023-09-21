4deaf300b3a53367bd9a5a440c85721aa5c63b8c
Rei-Ashine <52484735+Rei-Ashine@users.noreply.github.com>
2023-09-21 17:00:17 +0900

    Modify the script to count the number of patterns to be replaced

--- scripts/format_pdb_file.py
+++ scripts/format_pdb_file.py
@@ -25,33 +25,35 @@ def delete_lines(line, ng_words):
     return flag
 
 
+def count_patterns(pattern, line, logs):
+    """
+    Count the number of occurrences of a particular string.
+    """
+    target = re.findall(pattern, line)
+    assert len(target) <= 1, \
+        "[Warning] The line contains an unknown pattern, and the string cannot be replaced."
+
+    if len(target) == 1:
+        token = "".join(target[0])
+        if token in logs:
+            logs[token] += 1
+        else:
+            logs[token] = 1
+    return logs
+
+
 def replace_words(line, logs):
     """
     Replace a specific string in a given line.
     """
-    # Replace "CYSS" with "CYS "
-    cyss = re.findall("CYSS", line)
-    assert len(cyss) <= 1, \
-        "[Warning] The line contains an unknown pattern, \
-        and the string cannot be replaced."
-    if len(cyss) == 1:
-        logs["CYSS"] += 1
-
-    wo_cyss = re.sub("CYSS", "CYS ", line)
+    # Replace "CYSS[A..Z]" with "CYS [A..Z]"
+    pattern1 = re.compile(r"(CYSS)([A-Z]{1})")
+    logs = count_patterns(pattern1, line, logs)
+    wo_cyss = pattern1.sub(r"CYS \2", line)
 
     # Replace "+/-[A..Z]" with " [A..Z]"
-    pattern1 = re.compile(r"[\+\-][A-Z]{1}")
-    target = re.findall(pattern1, wo_cyss)
-    assert len(target) <= 1, \
-        "[Warning] The line contains an unknown pattern, \
-        and the string cannot be replaced."
-    if len(target) == 1:
-        if target[0] in logs:
-            logs[target[0]] += 1
-        else:
-            logs[target[0]] = 1
-
     pattern2 = re.compile(r"([\+\-])([A-Z]{1})")
+    logs = count_patterns(pattern2, line, logs)
     replaced = pattern2.sub(r" \2", wo_cyss)
 
     return replaced, logs
@@ -63,7 +65,7 @@ def formatter(input_moldesk, output_file, ng_words):
     """
     count_lines = 0
     count_deletion = 0
-    logs = {"CYSS": 0}
+    logs = dict()
     with open(output_file, encoding="utf-8", mode="w") as file:
         for line in open(input_moldesk, encoding="utf-8", mode="r"):
 
@@ -81,6 +83,7 @@ def formatter(input_moldesk, output_file, ng_words):
     pprint.pprint(logs)
     total = sum(logs.values())
     print(f"[INFO] Total number of replaced strings : {total}")
+    print(f"[INFO] {len(logs)} different string patterns in the file needed to be replaced.")
 
     rest = count_lines - count_deletion
     print(f"[INFO] Output PDB file name : {os.path.basename(output_file)}")
