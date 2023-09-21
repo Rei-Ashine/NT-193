542e9eca6d4fddb5f05cc40330be3cf7d636ad5d
Rei-Ashine <52484735+Rei-Ashine@users.noreply.github.com>
2023-09-21 11:31:48 +0900

    second commit

---
+++ DUMP/test_format_pdb_file.py
@@ -0,0 +1,60 @@
+# DATE: Sep. 20th, 2023
+# Author: rei-ashine
+
+"""
+Format a PDB file from MolDesk to a PDB file that PDBePISA can interpret.
+"""
+
+import os
+import os.path
+import argparse
+
+
+def delete_lines(input_moldesk, output_file, words):
+    """
+    Remove lines containing a specific string from a file.
+    """
+    count_lines = 0
+    count_deletion = 0
+    for line in open(input_moldesk, encoding="utf-8", mode="r"):
+
+        count_lines += 1
+        for i in words:
+            if i in line:
+                count_deletion += 1
+                break
+        else:
+            with open(output_file, encoding="utf-8", mode="a") as file:
+                file.write(line)
+
+    print(f"[INFO] The input PDB file has {count_lines} lines.")
+    print(f"[INFO] {count_deletion} lines are deleted.")
+
+    rest = count_lines - count_deletion
+    print(f"[INFO] Output PDB file name : {os.path.basename(output_file)}")
+    print(f"[INFO] The output PDB file has {rest} lines.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", required=True, help="Set the input PDB file path")
+    parser.add_argument("-o", "--output", default="Formatted", help="Set the output directory name")
+    args = parser.parse_args()
+    print("----- START -----")
+
+    # Load an input PDB file
+    path = args.input
+    basename = os.path.basename(path)
+    print(f"[INFO] Input PDB file name : {basename}")
+
+    if not os.path.isdir(args.output):
+        os.makedirs(args.output)
+    filename = os.path.splitext(os.path.basename(path))[0]
+    path_output = f"{args.output}/{filename}_formatted.pdb"
+    assert not os.path.isfile(path_output), "[Warning] The input PDB file is already formatted."
+
+    # Format a PDB file
+    NG_words = ["SSBOND"]
+    delete_lines(path, path_output, NG_words)
+
+    print("----- END -----")
