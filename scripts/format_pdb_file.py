23d657ba99d7ff1acc32572c7a8eb64a49c7a646
Rei-Ashine <52484735+Rei-Ashine@users.noreply.github.com>
2023-09-21 15:45:02 +0900

    Modify the script to replace "+/-[A..Z]" with " [A..Z]"

--- scripts/format_pdb_file.py
+++ scripts/format_pdb_file.py
@@ -9,6 +9,7 @@ Format a PDB file from MolDesk to a PDB file that PDBePISA can interpret.
 import os
 import os.path
 import re
+import pprint
 import argparse
 
 
@@ -16,20 +17,44 @@ def delete_lines(line, ng_words):
     """
     Remove lines containing a specific string from a file.
     """
-    status = False
+    flag = False
     for i in ng_words:
         if i in line:
-            status = True
+            flag = True
             break
-    return status
+    return flag
 
 
-def replace_words(line):
+def replace_words(line, logs):
     """
     Replace a specific string in a given line.
     """
+    # Replace "CYSS" with "CYS "
+    cyss = re.findall("CYSS", line)
+    assert len(cyss) <= 1, \
+        "[Warning] The line contains an unknown pattern, \
+        and the string cannot be replaced."
+    if len(cyss) == 1:
+        logs["CYSS"] += 1
+
     wo_cyss = re.sub("CYSS", "CYS ", line)
-    return wo_cyss
+
+    # Replace "+/-[A..Z]" with " [A..Z]"
+    pattern1 = re.compile(r"[\+\-][A-Z]{1}")
+    target = re.findall(pattern1, wo_cyss)
+    assert len(target) <= 1, \
+        "[Warning] The line contains an unknown pattern, \
+        and the string cannot be replaced."
+    if len(target) == 1:
+        if target[0] in logs:
+            logs[target[0]] += 1
+        else:
+            logs[target[0]] = 1
+
+    pattern2 = re.compile(r"([\+\-])([A-Z]{1})")
+    replaced = pattern2.sub(r" \2", wo_cyss)
+
+    return replaced, logs
 
 
 def formatter(input_moldesk, output_file, ng_words):
@@ -38,6 +63,7 @@ def formatter(input_moldesk, output_file, ng_words):
     """
     count_lines = 0
     count_deletion = 0
+    logs = {"CYSS": 0}
     with open(output_file, encoding="utf-8", mode="w") as file:
         for line in open(input_moldesk, encoding="utf-8", mode="r"):
 
@@ -45,11 +71,16 @@ def formatter(input_moldesk, output_file, ng_words):
             if delete_lines(line, ng_words):
                 count_deletion += 1
             else:
-                line = replace_words(line)
+                line, logs = replace_words(line, logs)
                 file.write(line)
 
     print(f"[INFO] The input PDB file has {count_lines} lines.")
-    print(f"[INFO] {count_deletion} lines are deleted.")
+    print(f'[INFO] {count_deletion} lines that contain the "SSBOND" line are deleted.')
+
+    print("[INFO] Replaced strings and the number of occurrences")
+    pprint.pprint(logs)
+    total = sum(logs.values())
+    print(f"[INFO] Total number of replaced strings : {total}")
 
     rest = count_lines - count_deletion
     print(f"[INFO] Output PDB file name : {os.path.basename(output_file)}")
